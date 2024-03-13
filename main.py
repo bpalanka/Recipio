e591add7798c04d6c1aac3bc4472864d739d37de
AhnafTalukder <talukderaxif@gmail.com>
2024-03-13 18:59:48 -0400

    something

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, url_for
+from allrecipes import AllRecipes
 
 app = Flask(__name__)
 
+query_result = AllRecipes.search("rice tomato chicken")
+
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', recipes=query_result)
 
 if __name__ == '__main__':
     app.run(debug=True)
