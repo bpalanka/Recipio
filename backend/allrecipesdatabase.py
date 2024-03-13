b515d433770e2c7a54f305fd24f6b72ef0fc66be
bpalanka <bela.palankar@gmail.com>
2024-03-13 18:28:03 -0400

    added experiment.py code

--- backend/allrecipesdatabase.py
+++ backend/allrecipesdatabase.py
@@ -1,4 +1,6 @@
 from allrecipes import AllRecipes
+# import pandas as pd
+import pandas as pd
 
 # Ask for ingredients
 def ingredientInput(ingredients):
@@ -59,19 +61,19 @@ else:
 
 # Search:
 #search_string = "chicken garam masala"  # Query
-    
-query_result = AllRecipes.search(query)
+#search_string = "pork curry"  # Query
+query_result = AllRecipes.search(ingredients)
 
 # Get:
 main_recipe_url = query_result[0]['url']
 detailed_recipe = AllRecipes.get(main_recipe_url)  # Get the details of the first returned recipe (most relevant in our case)
 
-# Display result:
-print("## %s:" % detailed_recipe['name'])  # Name of the recipe
+ 
+# Calling DataFrame constructor on list
+df = pd.DataFrame(query_result)
+print(df)
 
-print("### For %s servings:" % detailed_recipe['nb_servings'])
-for ingredient in detailed_recipe['ingredients']:  # List of ingredients
-    print("- %s" % ingredient)
+filtered_data = df[['name', 'rate']]
+print(filtered_data)
+query_result = AllRecipes.search(query)
 
-for step in detailed_recipe['steps']:  # List of cooking steps
-    print("# %s" % step)
