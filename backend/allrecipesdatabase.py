f1e735fb6d79f50e5190ef4718628555996f987a
David Zhou <dzhou6@gmu.edu>
2024-10-26 16:20:19 -0400

    Added image recognition module Recipio.py utilizing gemini 1.5 flash Modified allrecipesdatabase.py with additional control flow to account for image input Added spices.jpeg for testing purposes

--- backend/allrecipesdatabase.py
+++ backend/allrecipesdatabase.py
@@ -1,44 +1,55 @@
 from allrecipes import AllRecipes
 import pandas as pd
-
-def getIngredientList(prompt, include_measurement=True):
+import Recipio as rec
+import sys
+def getIngredientList(prompt, image,include_measurement=True):
     """Ask user for ingredients repeatedly to construct a list.
     param: prompt String that will ask user if they want to include or exclude ingredients.
     param: include_measurement Boolean to determine if measurements are needed.
     """
     print(prompt)
     user_dict = {}
-    
-    while True:
-        ingredient = input("Enter ingredient (or press Enter to stop): ")
-        if not ingredient:  # Stop if ingredient is empty
-            print("Ending list...")
-            break
+    #if image recognition was not used
+    if(not image):
+        while True:
+            ingredient = input("Enter ingredient (or press Enter to stop): ")
+            if not ingredient:  # Stop if ingredient is empty
+                print("Ending list...")
+                break
 
         # Ask for measurement if including ingredients
-        if include_measurement:
-            while True:
-                measurement = input("Enter measurement for {ingredient} (include units): ")
-                if measurement:  # Only accept non-empty measurement
-                    break
-                print("Measurement cannot be empty. Please provide a value.")
-        else:
-            measurement = None  # No measurement for excluded ingredients
+            if include_measurement:
+                while True:
+                    measurement = input("Enter measurement for {ingredient} (include units): ")
+                    if measurement:  # Only accept non-empty measurement
+                        break
+                    print("Measurement cannot be empty. Please provide a value.")
+            else:
+                measurement = None  # No measurement for excluded ingredients
         
         # Add to the dictionary
-        user_dict[ingredient] = measurement
-    
+            user_dict[ingredient] = measurement
+    #if image recognition was used
+    else:
+        i=0
+        #while loop that loops through every other index starting with 0
+        while(i<len(image)-1):
+            user_dict[image[i]]=image[i+1]
+            i+=2
     return user_dict
-
 def main():
     print("Welcome to Recip.io!")
-
+    '''image=True
+    if(image):
+        arr=rec.imager(THE_IMAGE_HERE)'''
+    #run the below code if there's an image passed
+    image=rec.preload()
     # Get ingredients to include
-    ingredients_incl = getIngredientList("Please start typing your ingredients and measurements below (hit enter twice to end your list)", include_measurement=True)
+    ingredients_incl = getIngredientList("Please start typing your ingredients and measurements below (hit enter twice to end your list)",image, include_measurement=True)
     print("Ingredients entered:", ingredients_incl)
 
     # Get ingredients to exclude
-    ingredients_excl = getIngredientList("Please start typing the ingredients you want to exclude (hit enter twice to end your list)", include_measurement=False)
+    ingredients_excl = getIngredientList("Please start typing the ingredients you want to exclude (hit enter twice to end your list)",False, include_measurement=False)
     print("Ingredients excluded:", ingredients_excl)
 
     # If the ingredient list is empty, ask again
